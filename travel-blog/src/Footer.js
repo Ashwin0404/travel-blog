7499f22b2c998c9c4e76044d8a86e5c2961fd8ab
Your Name <you@example.com>
2024-04-18 20:08:10 +0530

    Portfolio added

--- travel-blog/src/Footer.js
+++ travel-blog/src/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <caption>2024 © Ashwin Anand. All Rights Reserved.</caption>
       <caption className='footer-caption'>
         <div id="details">
-		<a href="https://ashwin0404.github.io/portfolio" target="_blank" rel="noreferrer noopener">GitHub</a> | {' '} 
+		<a href="https://ashwin0404.github.io/portfolio" target="_blank" rel="noreferrer noopener">GitHub/Portfolio</a> | {' '} 
         <a href="https://www.linkedin.com/in/ashwin-anand-b85462265" target="_blank" rel="noreferrer noopener">LinkedIn</a>
 		</div>
       </caption>
